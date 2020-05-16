ae4fbd2a3d6e643efbc38ecc76402ab4f684e0ea
RichardPoulson <contact@richardpoulson.com>
2020-05-16 16:32:13 -0700

    Copied tests from flask example
    https://github.com/pallets/flask/tree/1.1.2/examples/tutorial

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ setup(
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     extras_require={  # Optional
-        'dev': ['check-manifest'],
+        'dev': ['check-manifest', 'wheel'],
         'test': ['coverage', 'pytest'],
     },
 
